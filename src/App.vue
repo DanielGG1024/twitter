2cf775ea25b97afa5d6da9b0dbf20a994d501a79
DanielGG1024 <a22472510@gmail.com>
2021-09-13 17:12:24 +0800

    modify basic environment

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+//引入全域變數
 @import 'assets/scss/main.scss';
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: $mainColor;
-}
-
-#nav a.router-link-exact-active {
-  color: $mainColor;
-}
 </style>
