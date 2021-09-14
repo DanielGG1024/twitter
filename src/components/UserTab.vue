2c0b357793202514c262c32eac41f1d97dc974b7
reDawsonLin <redawson357@gmail.com>
2021-09-14 17:45:23 +0800

    add usertab

--- src/components/UserTab.vue
+++ src/components/UserTab.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    
+  <div id="user-tabs" >
+    <div class="tab user-tweets clicked">推文</div>
+    <div class="tab user-replies">推文與回覆</div>
+    <div class="tab user-likes">喜歡的內容</div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  @import "@/assets/scss/userTab.scss"
+</style>
