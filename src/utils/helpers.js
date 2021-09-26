c18961db4e2a955d29891be89ea7d8ae17f6db77
DanielGG1024 <a22472510@gmail.com>
2021-09-26 23:30:53 +0800

    publicMessage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,11 +4,14 @@ import Swal from 'sweetalert2'
 const baseURL = 'https://tranquil-crag-64775.herokuapp.com/api/'
 // const baseURL = 'https://08c7-2402-7500-953-609f-29ec-88da-18f4-2869.ngrok.io/api'
 // const baseURL = "https://1411-219-85-165-164.ngrok.io/api/"
+// const socketURL ="https://tranquil-crag-64775.herokuapp.com/api/"
 
 export const apiHelper = axios.create({
     baseURL,
 })
-
+// export const apiSocketHelper = axios.create({
+//     socketURL,
+// })
 export const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
